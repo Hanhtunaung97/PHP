8914c95b04da0de2b5d43b7f7f9234f8892c27aa
Hanhtunaung97 <littlehanhtunaung97>
2024-09-27 17:25:38 +0800

    area calculator

---
+++ mini-projects/test.php
@@ -0,0 +1,36 @@
+<?php
+// touch("test.txt");
+// mkdir("test",0777,true);
+// var_dump(file_exists("test.txt"));
+// var_dump(file_exists("tests.txt"));
+// var_dump(is_dir("test"));
+// var_dump(is_dir("tests"));
+// var_dump(is_file("test.txt"));
+// var_dump(is_file("test"));
+// print_r(scandir("."));
+// unlink("test.txt");
+// rmdir("test");
+$fileName="test.txt";
+if(!file_exists($fileName)){
+    touch($fileName);
+}
+$fileStream=fopen($fileName,"r");
+// echo fread($fileStream,1024);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgetc($fileStream);
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+// echo fgets($fileStream);
+// var_dump(feof($fileStream));
+// while(feof($fileStream)==false){
+//     echo fgets($fileStream);
+// }
+// fclose($fileStream);
+// $fileStream=fopen($fileName,"a");
+// fwrite($fileStream,"Hello World");
+// fwrite($fileStream,"Hello spaces");
+// fclose($fileStream);
+// unlink("test.txt");
